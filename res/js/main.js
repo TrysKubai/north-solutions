d6e115baecf544e3f3dc5256d62d2d118b44f120
pliupelis <pliupelis@inbox.lt>
2022-06-14 18:13:42 +0300

    nav improvement

--- res/js/main.js
+++ res/js/main.js
@@ -1,12 +1,20 @@
 const hambuger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".side-navigation");
-const links = document.querySelectorAll(
-  ".side-navigation a, .right-side__language"
-);
+const links = document.querySelectorAll(".side-navigation a");
 const header = document.querySelector("header");
 const lines = document.querySelectorAll(".line");
 
 hambuger.addEventListener("click", () => {
+  navToggle();
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    navToggle();
+  });
+});
+
+navToggle = () => {
   lines[0].classList.toggle("topLine");
   lines[1].classList.toggle("invisible");
   lines[2].classList.toggle("bottomLine");
@@ -15,18 +23,4 @@ hambuger.addEventListener("click", () => {
   links.forEach((link) => {
     link.classList.toggle("fade");
   });
-});
-//collapsible nav
-let coll = document.getElementsByClassName("collapsible");
-
-for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+};
