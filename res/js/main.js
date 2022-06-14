bc00ee9df1d3879c5aadf9f08844fff6133a4c2f
pliupelis <pliupelis@inbox.lt>
2022-06-14 18:54:57 +0300

    services section stacking order bug

--- res/js/main.js
+++ res/js/main.js
@@ -3,17 +3,31 @@ const navLinks = document.querySelector(".side-navigation");
 const links = document.querySelectorAll(".side-navigation a");
 const header = document.querySelector("header");
 const lines = document.querySelectorAll(".line");
+const servicesContainer = document.querySelector(".services");
 
 hambuger.addEventListener("click", () => {
   navToggle();
+  serviceToggle();
 });
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
     navToggle();
+    serviceToggle();
   });
 });
-
+//service section hack >:D
+serviceToggle = () => {
+  console.log(servicesContainer.style.display);
+  if (
+    servicesContainer.style.display === "" ||
+    servicesContainer.style.display === "block"
+  ) {
+    servicesContainer.style.display = "none";
+  } else {
+    servicesContainer.style.display = "block";
+  }
+};
 navToggle = () => {
   lines[0].classList.toggle("topLine");
   lines[1].classList.toggle("invisible");
